be268cedd7daa00537be53ca0b0ca0b4efeb9ae5
Vishal Ravi Shankar <rsh.vishal@gmail.com>
2020-02-20 15:37:18 -0800

    Added shell script to create skeleton filesystem

---
+++ mk_skelfs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mkdir -p $HOME/filesystem/foo/bar/baz
+echo 'Hello foo' > $HOME/filesystem/foo/foo1
+echo 'Hello bar' > $HOME/filesystem/foo/bar/bar1
+echo 'Hello baz' > $HOME/filesystem/foo/bar/baz/baz1
