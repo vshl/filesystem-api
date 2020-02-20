a77f43e9730fc8a7b99622f6ecc1fc884908427a
Vishal Ravi Shankar <rsh.vishal@gmail.com>
2020-02-20 15:18:14 -0800

    Removed hardcoded paths

--- test_filesystem.py
+++ test_filesystem.py
@@ -1,5 +1,9 @@
 import pytest
 import api
+import os
+
+HOME = os.environ['HOME']
+path = os.path.join(HOME, 'filesystem/foo')
 
 @pytest.fixture
 def client():
@@ -9,20 +13,20 @@ def client():
 
 def test_get_all(client):
     response = client.get('/',
-            data=dict(path='/Users/vishal/filesystem/foo'))
+            data=dict(path=path))
     assert b'bar' in response.data
 
 def test_get_bar(client):
     response = client.get('/bar',
-            data=dict(path='/Users/vishal/filesystem/foo'))
+            data=dict(path=path))
     assert b'bar1' in response.data
 
 def test_get_foo1(client):
     response = client.get('/foo1',
-            data=dict(path='/Users/vishal/filesystem/foo'))
+            data=dict(path=path))
     assert b'Hello foo' in response.data
 
 def test_get_bar1(client):
     response = client.get('/bar/bar1',
-            data=dict(path='/Users/vishal/filesystem/foo'))
+            data=dict(path=path))
     assert b'Hello bar' in response.data
